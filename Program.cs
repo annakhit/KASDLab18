ceba283b63a300b49b502e12c825dce1c9c28301
Anna Khit <annakhit02@mail.ru>
2024-11-14 21:44:04 +0300

    Implemented MyHashMap Class

--- Program.cs
+++ Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KASDLab18
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            MyHashMap<string, int> hashMap = new MyHashMap<string, int>();
+
+            hashMap.Put("KASD", 4);
+
+            Console.WriteLine(hashMap.Get("KASD"));
+
+            Console.WriteLine(hashMap.ContainsKey("KASD"));
+            Console.WriteLine(hashMap.ContainsKey("ASD"));
+
+            hashMap.Put("KASD", 5);
+            hashMap.Put("TEST", 5);
+            hashMap.Put("DIFF", 5);
+
+            foreach (var entry in hashMap.EntrySet())
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", entry.Key, entry.Value);
+            }
+
+            Console.WriteLine(hashMap.IsEmpty());
+
+            hashMap.Remove("TEST");
+
+            foreach (var entry in hashMap.EntrySet())
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", entry.Key, entry.Value);
+            }
+
+            HashSet<string> keySet = hashMap.KeySet();
+
+            foreach (string key in keySet)
+            {
+                Console.WriteLine("Key: {0}", key);
+            }
+
+            Console.ReadKey();
         }
     }
 }
